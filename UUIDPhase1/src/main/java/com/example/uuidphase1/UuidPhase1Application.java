ab3e17ee1c002edea2f1c4fccb72f1ab973865f9
sailesh <saileshrambally01@gmail.com>
2024-07-30 15:26:20 +0200

    first commit

---
+++ UUIDPhase1/src/main/java/com/example/uuidphase1/UuidPhase1Application.java
@@ -0,0 +1,13 @@
+package com.example.uuidphase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuidPhase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UuidPhase1Application.class, args);
+	}
+
+}
---
+++ UUIDPhase1/src/test/java/com/example/uuidphase1/UuidPhase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uuidphase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UuidPhase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
